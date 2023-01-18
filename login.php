df3f2cf9f961eff169d8b3bdc972672baa95662c
กรรณิกา ตากิ่มนอก <6300408@kkfnets.kkf>
2023-01-18 10:00:59 +0700

    test test

---
+++ login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="./src/assets/bootstrap-5.3.0/bootstrap.min.css" rel="stylesheet">
+    <link href="./src/css/main.style" rel="stylesheet">
+
+  </head>
+  <body>
+
+  <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+
+    <script src="./src/assets/bootstrap-5.3.0/bootstrap.bundle.min.js"></script>
+    <script src="./src/assets/jquery-3.6.3/jquery-3.6.3.min.map"></script>
+    <script src="./src/js/main.js"></script>
+    
+  </body>
+</html>
