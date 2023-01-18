70ecc7072085286adb788cbc92db8d18a7933549
Rung <Rung@DESKTOP-1I1M1P5>
2023-01-18 23:47:36 +0700

    edit header

--- layout/head.php
+++ layout/head.php
@@ -6,192 +6,20 @@
     </div>
     <ul class="sidebar-menu p-3 m-0 mb-0">
         <li class="sidebar-menu-item active">
-            <a href="#">
+            <a href="/ReserveSpace/index.php">
                 <i class="ri-dashboard-line sidebar-menu-item-icon"></i>
-                Dashboard
+                จองพื้นที่ขาย
             </a>
         </li>
-        <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Custom</li>
-        <li class="sidebar-menu-item has-dropdown">
-            <a href="#">
-                <i class="ri-pages-line sidebar-menu-item-icon"></i>
-                Pages
-                <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-            </a>
-            <ul class="sidebar-dropdown-menu">
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        User Profile
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item has-dropdown">
-                    <a href="#">
-                        Blog
-                        <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-                    </a>
-                    <ul class="sidebar-dropdown-menu">
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Post
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-dropdown-menu-item has-dropdown">
-                    <a href="#">
-                        Email Template
-                        <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-                    </a>
-                    <ul class="sidebar-dropdown-menu">
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Reset Password
-                            </a>
-                        </li>
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Changed Password
-                            </a>
-                        </li>
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Verify Email
-                            </a>
-                        </li>
-                        <li class="sidebar-dropdown-menu-item">
-                            <a href="#">
-                                Invitation
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Pricing
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        About Us
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Contact Us
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Our Team
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="sidebar-menu-item has-dropdown">
-            <a href="#">
-                <i class="ri-user-line sidebar-menu-item-icon"></i>
-                Authentication
-                <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-            </a>
-            <ul class="sidebar-dropdown-menu">
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Login
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Registration
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Reset Password
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Change Password
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Confirm Password
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Deactivate Account
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="sidebar-menu-item has-dropdown">
-            <a href="#">
-                <i class="ri-window-line sidebar-menu-item-icon"></i>
-                Widgets
-                <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-            </a>
-            <ul class="sidebar-dropdown-menu">
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Charts
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Tables
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Apps</li>
-        <li class="sidebar-menu-item has-dropdown">
-            <a href="#">
-                <i class="ri-shopping-cart-2-line sidebar-menu-item-icon"></i>
-                eCommerce
-                <i class="ri-arrow-down-s-line sidebar-menu-item-accordion ms-auto"></i>
-            </a>
-            <ul class="sidebar-dropdown-menu">
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Shop
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Checkout
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Details
-                    </a>
-                </li>
-                <li class="sidebar-dropdown-menu-item">
-                    <a href="#">
-                        Wishlist
-                    </a>
-                </li>
-            </ul>
-        </li>
+        <?php if($user["ur_Id"] == "R002"){ ?>
+        <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Admin</li>
         <li class="sidebar-menu-item">
-            <a href="#">
-                <i class="ri-mail-line sidebar-menu-item-icon"></i>
-                Email
+            <a href="/ReserveSpace/signup.php">
+                <i class="ri-user-add-line sidebar-menu-item-icon"></i>
+                เพิ่มชื่อผู้ใช้
             </a>
         </li>
-        <li class="sidebar-menu-item">
-            <a href="#">
-                <i class="ri-calendar-line sidebar-menu-item-icon"></i>
-                Calendar
-            </a>
-        </li>
+        <?php } ?>
     </ul>
 </div>
 <div class="sidebar-overlay"></div>
