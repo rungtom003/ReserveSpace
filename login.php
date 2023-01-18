c565a3c85ba4b757b6e7db72641a65d18edb68f1
กรรณิกา ตากิ่มนอก <6300408@kkfnets.kkf>
2023-01-18 11:29:26 +0700

    test test

--- login.php
+++ login.php
@@ -10,30 +10,27 @@
   </head>
   <body>
 
-<div class="d-flex justify-content-center vh-100 vw-100 align-items-center">
-  <div class="container-fluid text-center">
-    <div class="d-flex justify-content-center align-items-center">
-      <div class="col-6">
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" >
+    <div class="d-flex justify-content-center vh-100 vw-100 align-items-center">
+      <div class="container-fluid text-center">
+
+        <div class="d-flex justify-content-center align-items-center">
+          <div class="col-6">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" >
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
+        
       </div>
     </div>
-    
-
-        
-    
-
-  </div>
-</div>
 
     
 
