82ba98a0eede79b31969e3b5817c14722092c9bf
rungtom003 <rungchai990@gmail.com>
2023-01-18 10:04:50 +0700

    update css

--- backend/connectdb.php
+++ backend/connectdb.php
@@ -2,9 +2,11 @@
 $servername = "45.144.164.52:13306";
 $username = "root";
 $password = "Rung_tom003";
+$database = "reserve_space";
+$port = "13306";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password,);
 
 // Check connection
 if (!$conn) {
