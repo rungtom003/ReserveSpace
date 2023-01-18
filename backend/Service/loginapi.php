077854d5afe49a9d76c389045d499f4af3dfc04d
rungtom003 <rungchai990@gmail.com>
2023-01-18 13:26:08 +0700

    update service

---
+++ backend/Service/loginapi.php
@@ -0,0 +1,31 @@
+<?php
+include "../class/resp.php";
+include "connectdb.php";
+
+$resp = new Resp();
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if ($connect_status == "success") {
+        $username = $_POST["u_Username"];
+        $password = $_POST["u_Password"];
+
+        $sql = "SELECT * FROM reserve_space.tb_user where u_Username = '" . $username . "' and u_Password = '" . $password . "';";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $resp->data = $row;
+        }
+
+        $resp->set_message("connection database success.");
+        $resp->set_status("success");
+    } else {
+        $resp->set_message("connection database fail.");
+        $resp->set_status("fail");
+    }
+}
+else
+{
+    $resp->set_message("Request method fail.");
+    $resp->set_status("");
+}
+
+echo json_encode($resp);
--- index.php
+++ index.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="./src/assets/bootstrap-5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./src/assets/sweetalert2-11.7.0/sweetalert2.min.css" rel="stylesheet">
     <link href="./src/css/main.style" rel="stylesheet">
 
   </head>
   <body>
     <h1>Hello, world! asdfasdfasdf</h1>
     <script src="./src/assets/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="./src/assets/sweetalert2-11.7.0/sweetalert2.min.js"></script>
     <script src="./src/assets/jquery-3.6.3/jquery-3.6.3.min.js"></script>
     <script src="./src/js/main.js"></script>
     
