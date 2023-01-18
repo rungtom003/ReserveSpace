2aba645280d790b5d88bbf46124a46b57c980dcb
Chettha Golf <Chetthagolf@gmail.com>
2023-01-18 13:04:41 +0700

    Update login

--- forms.php
+++ forms.php
@@ -6,22 +6,19 @@
     <title>Bootstrap demo</title>
     <link href="./src/assets/bootstrap-5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="./src/css/main.style" rel="stylesheet">
+    <link href="./src/css/sidebars.css">
 
+ 
   </head>
-  <!---->
+  
   <body>
-    <!--begin::navbar-->
-    <!--end:: navbar-->
 
-    <!--begin::Content-->
-    <h1>Hello, world! asdfasdfasdf</h1>
-    <!--end::Content-->
-
-    <!--begin::-->
-    <!--end::-->
 
     <script src="./src/assets/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="./src/assets/jquery-3.6.3/jquery-3.6.3.min.js"></script>
     <script src="./src/js/main.js"></script>
+    <script src="./src/js/sidebars.js"></script>
+
+    
   </body>
 </html>
--- login.php
+++ login.php
@@ -6,36 +6,35 @@
     <title>Bootstrap demo</title>
     <link href="./src/assets/bootstrap-5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="./src/css/main.style" rel="stylesheet">
-
+    <link href="./src/css/sign-in.css" rel="stylesheet">
   </head>
-  <body>
 
-    <div class="d-flex justify-content-center vh-100 vw-100 align-items-center">
-      <div class="container-fluid text-center">
 
-        <div class="d-flex justify-content-center align-items-center">
-          <div class="col-6">
-            <form>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" >
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
-        
-      </div>
+  <body class="text-center">
+
+  <main class="form-signin w-100 m-auto">
+  <form>
+    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
     </div>
 
-    
-
-    
-
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+  </form>
+</main>
 
     <script src="./src/assets/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="./src/assets/jquery-3.6.3/jquery-3.6.3.min.js"></script>
