4a310a6d94dae0bcc90c24356afd21e367ca78b5
rungtom003 <rungchai990@gmail.com>
2023-02-21 14:23:33 +0700

    add u_IdWalkin

--- backend/Service/data_reserve.php
+++ backend/Service/data_reserve.php
@@ -6,7 +6,7 @@ $resp = new Resp();
 $dataUsers = array();
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     if ($connect_status == "success") {
-        $sql = "SELECT a.r_Id,a.r_Note,a.r_Status,a.r_DateTime,b.a_Id,b.a_Name,b.a_ReserveStatus,c.z_Name,d.u_FirstName,d.u_LastName,d.u_CardNumber,d.u_Phone,d.u_Prefix,d.u_Birthday,d.u_Address,d.u_Road,d.u_SubDistrict,d.u_District,d.u_Province,d.u_ShopName,d.u_ProductName FROM kkmuni_street.tb_reserve as a  INNER JOIN kkmuni_street.tb_area as b ON a.a_Id = b.a_Id INNER JOIN kkmuni_street.tb_zone as c ON b.z_Id = c.z_Id INNER JOIN kkmuni_street.tb_user as d ON d.u_Id = a.u_Id ;";
+        $sql = "SELECT d.u_IdWalkin,a.r_Id,a.r_Note,a.r_Status,a.r_DateTime,b.a_Id,b.a_Name,b.a_ReserveStatus,c.z_Name,d.u_FirstName,d.u_LastName,d.u_CardNumber,d.u_Phone,d.u_Prefix,d.u_Birthday,d.u_Address,d.u_Road,d.u_SubDistrict,d.u_District,d.u_Province,d.u_ShopName,d.u_ProductName FROM kkmuni_street.tb_reserve as a  INNER JOIN kkmuni_street.tb_area as b ON a.a_Id = b.a_Id INNER JOIN kkmuni_street.tb_zone as c ON b.z_Id = c.z_Id INNER JOIN kkmuni_street.tb_user as d ON d.u_Id = a.u_Id ;";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
@@ -52,6 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
                     'u_Province' => $row['u_Province'],
                     'u_ShopName' => $row['u_ShopName'],
                     'u_ProductName' => $row['u_ProductName'],
+                    'u_IdWalkin' => $row['u_IdWalkin'],
                     'status' => $status
                 );
                 array_push($dataUsers,$arrCustom);
